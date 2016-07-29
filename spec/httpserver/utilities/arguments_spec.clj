f8d70474eef8bd03c5d12c4efdfe7a648fd02824
Tyler Decker <tyler.decker@gmail.com>
2016-07-29 16:37:52 -0500

    fixed form order

--- spec/httpserver/utilities/arguments_spec.clj
+++ spec/httpserver/utilities/arguments_spec.clj
@@ -7,14 +7,16 @@
 (def argsEmpty '())
 
 (describe "arguments"
-  (it "sets port to 5000"
-    (let  [args  ["-p" "5000" "-d" "./site"]]
+
+  (let  [args  ["-p" "5000" "-d" "./site"]]
+    (it "sets port to 5000"
       (let [{:keys [options]} (arguments/build-map args)]
         (should= 5000 (:port options)))
 
       (it "set directory to ./site"
         (let [{:keys [options]} (arguments/build-map args)]
-          (should= "./site" (:directory options)))))
+          (should= "./site" (:directory options))))))
+
     (let [empty-args '()]
       (it "default port is 8080"
         (let [{:keys [options]} (arguments/build-map argsEmpty)]
@@ -22,4 +24,4 @@
 
       (it "default directory is ./"
         (let [{:keys [options]} (arguments/build-map argsEmpty)]
-          (should= "./" (:directory options)))))))
+          (should= "./" (:directory options))))))
