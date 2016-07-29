3dadf4b4150f316fa983017d9677388f0e25d07c
Tyler Decker <tyler.decker@gmail.com>
2016-07-29 13:31:44 -0500

    Core loads server with passed in command line args
    The core now loads the server, adds the teapot route and runs.

--- src/httpserver/core.clj
+++ src/httpserver/core.clj
@@ -1,6 +1,15 @@
-(ns httpserver.core)
+(ns httpserver.core
+  (:require [httpserver.handlers.teapot :as myteapot]
+            [httpserver.utilities.arguments :as argumenetParser]))
 
-(def display_welcome "Http Server!")
+(import '(com.td.HttpServer HttpServer))
+(import '(com.td.HttpServer HttpResponse))
+(import '(com.td.HttpServer IHandler))
 
-(defn -main []
-  (println display_welcome))
+
+(defn -main [& args]
+  (let [{:keys [options]} (argumenetParser/buildMap args)]
+    (let [server (HttpServer. (:port options) (:directory options))
+          myTea (myteapot/new-teapotHandler)]
+      (.addRoute server "GET" "/teapot" myTea)
+      (.run server))))
