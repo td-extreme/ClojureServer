a6086031f5fba83f462bc30cd4c6321dabcee2f0
Tyler Decker <tyler.decker@gmail.com>
2016-07-29 13:52:22 -0500

    Put display_welcome back

--- src/httpserver/core.clj
+++ src/httpserver/core.clj
@@ -1,13 +1,16 @@
 (ns httpserver.core
   (:require [httpserver.handlers.teapot :as myteapot]
-            [httpserver.utilities.arguments :as argumenetParser]))
+            [httpserver.utilities.arguments :as argumenetParser])
+  (:gen-class))
 
 (import '(com.td.HttpServer HttpServer))
 (import '(com.td.HttpServer HttpResponse))
 (import '(com.td.HttpServer IHandler))
 
+(def display_welcome "Http Server!")
 
 (defn -main [& args]
+  (println display_welcome)
   (let [{:keys [options]} (argumenetParser/buildMap args)]
     (let [server (HttpServer. (:port options) (:directory options))
           myTea (myteapot/new-teapotHandler)]
