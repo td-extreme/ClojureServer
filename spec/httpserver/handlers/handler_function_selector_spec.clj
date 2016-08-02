29935306a2ae1d1ff177fc520ee6bda026a56703
Tyler Decker <tyler.decker@gmail.com>
2016-08-02 18:15:42 -0500

    Created a function-selector.

---
+++ spec/httpserver/handlers/handler_function_selector_spec.clj
@@ -0,0 +1,14 @@
+(ns httpserver.handler_function_selector_spec
+  (:require [speclj.core :refer :all]
+            [httpserver.handlers.handler_function_selector :as selector]
+            [httpserver.handlers.teapot :as teapot]
+            [httpserver.handlers.ok :as ok]))
+
+(describe "routing"
+  (it "picks the ok route for GET /tea"
+    (let [request (hash-map :method "GET" :path "/tea")]
+      (should= ok/call (selector/get-function request))))
+
+  (it "picks the teapot route for GET /coffee"
+    (let [request (hash-map :method "GET" :path "/coffee")]
+      (should= teapot/call (selector/get-function request)))))
