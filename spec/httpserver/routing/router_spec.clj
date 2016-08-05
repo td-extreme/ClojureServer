8ff67829f23f75f2656f916fee4bbe7b99b7d6a2
Tyler Decker <tyler.decker@gmail.com>
2016-08-05 17:54:17 -0500

    Made helper functions private
    Removed tests for functions that are now private.

--- spec/httpserver/routing/router_spec.clj
+++ spec/httpserver/routing/router_spec.clj
@@ -5,26 +5,9 @@
 
 (import '(com.td.HttpServer HttpServer))
 
-(describe "Checking if this is the route it should be looking for"
-  (it "returns false if the method of the current route being checked does not match the method it should be looking for"
-    (should= false (router/check-route "PUT" "/path" {:method "GET" :path "/path"})))
-  (it "returns false if the path of the current route being checked does not match the path it should be looking for"
-    (should= false (router/check-route "GET" "/" {:method "GET" :path "/path"})))
-  (it "returns false if neither the method nor path do not match the ones it should be looking for"
-    (should= false (router/check-route "PUT" "/" {:method "GET" :path "/path"})))
-  (it "returns true if the method and path match the ones it should be looking for"
-    (should= true (router/check-route "GET" "/path" {:method "GET" :path "/path"}))))
-
-(describe "Finding our route inside a list of routes"
-  (it "returns a map that contains the route and path that it should be looking for"
-    (let [test-list (list {:method "GET" :path "/"}
-                          {:method "GET" :path "/test"}
-                          {:method "PUT" :path "/test"})]
-      (should= {:method "GET" :path "/test"} (router/find-route "GET" "/test" test-list)))))
-
 (describe "Geting the handler function"
   (it "returns the appropriate handler function for the route requested"
-    (let [test-list (list {:method "GET" :path "/" :handler "GET / hanlder"}
-                          {:method "GET" :path "/test" :handler "GET /test handler"})
+    (let [test-list [{:method "GET" :path "/" :handler "GET / hanlder"}
+                     {:method "GET" :path "/test" :handler :mock-test-handler}]
           request {:method "GET" :path "/test"}]
-      (should= "GET /test handler" (router/get-handler request test-list)))))
+      (should= :mock-test-handler (router/get-handler request test-list)))))
--- src/httpserver/routing/router.clj
+++ src/httpserver/routing/router.clj
@@ -1,15 +1,13 @@
 (ns httpserver.routing.router)
 
-(defn check-route [method path route]
+(defn- check-route [method path route]
   (and (= (:method route) method) (= (:path route) path)))
 
-(defn find-route [method path routes]
+(defn- find-route [method path routes]
   (some #(when (check-route method path %) %) routes))
 
-(defn get-handler [request routes]
-  (:handler (find-route (:method request) (:path request) routes))
-  )
+(defn get-handler [{:keys [method path]} routes]
+  (:handler (find-route method path routes)))
 
 (defn add-routes [server list-of-routes main-handler]
-  (doseq [route list-of-routes]
-    (.addRoute server (:method route) (:path route) main-handler)))
+  (run! #(.addRoute server (:method %) (:path %) main-handler) list-of-routes))
