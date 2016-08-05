2dfefbaaa67cb905ece68809da74a658cc6dfa6a
Tyler Decker <tyler.decker@gmail.com>
2016-08-05 15:14:10 -0500

    Moved routes to sperate ns
    This is to seperate the creation of routes from main.

--- src/httpserver/core.clj
+++ src/httpserver/core.clj
@@ -2,9 +2,7 @@
   (:require [httpserver.handlers.handler :as handler]
             [httpserver.utilities.arguments :as arguments]
             [httpserver.routing.router :as router]
-            [httpserver.handlers.teapot :as teapot]
-            [httpserver.handlers.ok :as ok]
-            )
+            [httpserver.routes.routes :as routes])
   (:gen-class))
 
 (import '(com.td.HttpServer HttpServer))
@@ -12,8 +10,6 @@
 (defn -main [& args]
   (let [{:keys [options]} (arguments/build-map args)]
     (let [server (HttpServer. (:port options) (:directory options))
-          routes (list {:method "GET" :path "/coffee" :handler teapot/call}
-          {:method "GET" :path "/tea" :handler ok/call})
-          main-handler (handler/new-handler routes)]
-      (router/add-routes server routes main-handler)
+          main-handler (handler/new-handler routes/list-of-routes)]
+      (router/add-routes server routes/list-of-routes main-handler)
       (.run server))))
---
+++ src/httpserver/routes/routes.clj
@@ -0,0 +1,6 @@
+(ns httpserver.routes.routes
+  (:require [httpserver.handlers.teapot :as teapot]
+            [httpserver.handlers.ok :as ok]))
+
+(def list-of-routes (list {:method "GET" :path "/coffee" :handler teapot/call}
+                          {:method "GET" :path "/tea" :handler ok/call}))
