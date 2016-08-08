61799f47bfb0860d65b295df9669ea0bff4c6d32
Tyler Decker <tyler.decker@gmail.com>
2016-08-08 12:01:38 -0500

    Added spaces around = in test

--- spec/httpserver/handlers/parameter_decode_spec.clj
+++ spec/httpserver/handlers/parameter_decode_spec.clj
@@ -3,6 +3,6 @@
             [httpserver.handlers.parameter-decode :as parameter-decode]))
 
 (describe "calling the handler"
-  (it "returns a map with :body 'var_1 = @\nvar_2=>' with arguments 'var_1 = %40&var_2=%3E"
-    (let [request {:method "GET" :path "parameters" :body "" :parameters "var_1 = %40&var_2=%3E"}]
-      (should= "var_1 = @\nvar_2=>" (:body (parameter-decode/call request))))))
+  (it "returns a map with :body 'var_1 = @\nvar_2 = >' with arguments 'var_1 = %40&var_2 = %3E"
+    (let [request {:method "GET" :path "parameters" :body "" :parameters "var_1 = %40&var_2 = %3E"}]
+      (should= "var_1 = @\nvar_2 = >" (:body (parameter-decode/call request))))))
