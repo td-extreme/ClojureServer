cacadb28c90fefeaa4a979e7883818dde04a12bd
Tyler Decker <tyler.decker@gmail.com>
2016-08-09 17:39:39 -0500

    Added options routes

---
+++ spec/httpserver/handlers/options_spec.clj
@@ -0,0 +1,11 @@
+(ns httpserver.options_spec
+  (:require [speclj.core :refer :all]
+            [httpserver.handlers.options :as options]))
+
+(describe "options"
+  (it "returns a map with :body ''"
+    (should= "" (:body (options/call :dummy-request))))
+  (it "returns a map with :code 200"
+    (should= 200 (:code (options/call :dummy-request))))
+  (it "returns a map with :header with a map of 'Allow' 'GET,HEAD,POST,OPTIONS,PUT'"
+    (should= {"Allow" "GET,HEAD,POST,OPTIONS,PUT"} (:headers (options/call :dummy-request)))))
--- src/httpserver/routing/routes.clj
+++ src/httpserver/routing/routes.clj
@@ -2,8 +2,12 @@
   (:require [httpserver.handlers.teapot :as teapot]
             [httpserver.handlers.ok :as ok]
             [httpserver.handlers.parameter-decode :as parameter-decode]
+            [httpserver.handlers.options :as options]
+            [httpserver.handlers.options_two :as options_two]
             ))
 
 (def list-of-routes (list {:method "GET" :path "/coffee" :handler teapot/call}
                           {:method "GET" :path "/tea" :handler ok/call}
-                          {:method "GET" :path "/parameters" :handler parameter-decode/call}))
+                          {:method "GET" :path "/parameters" :handler parameter-decode/call}
+                          {:method "OPTIONS" :path "/method_options" :handler options/call}
+                          {:method "OPTIONS" :path "/method_options2" :handler options_two/call}))
