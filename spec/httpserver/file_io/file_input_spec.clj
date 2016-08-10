6f8f6419dadc0150ce5d11160af34fb367d5d6ac
Tyler Decker <tyler.decker@gmail.com>
2016-08-10 14:39:35 -0500

    Add file type checking and file input

---
+++ spec/httpserver/file_io/file_input_spec.clj
@@ -0,0 +1,21 @@
+(ns httpserver.file-io-spec
+ (:require [speclj.core :refer :all]
+           [httpserver.file-io.file-input :as file-input]))
+
+(describe "file io"
+  (describe "reading a file"
+    (it "returns contents of testfile as byte-array"
+        (should= (type (byte-array 3)) (type (file-input/get-file-contents-as-bytes
+                              "./folder_for_io_testing"
+                              "/test.txt"))))
+   (it "returns a byte-array of length 32 when asked to open test.txt file"
+     (should= 33 (count (file-input/get-file-contents-as-bytes
+                            "./folder_for_io_testing"
+                            "/test.txt")))))
+
+  (describe "partial content"
+    (it "return a byte-array of the sting 'file' for range 8 12"
+      (should= "file" (apply str (map char (file-input/get-file-partial-content
+                                              "./folder_for_io_testing"
+                                              "/test.txt"
+                                              8 12)))))))
--- spec/httpserver/file_io/file_io_spec.clj
+++
@@ -1,9 +0,0 @@
-(ns httpserver.file-io-spec
- (:require [speclj.core :refer :all]
-           [httpserver.file-io.file-io :as file-io]))
-
-(describe "file io"
-  (describe "partial content"
-    (it "returns a subvector of [3 4 5] for arguemnts [1, 2, 3, 4, 5, 6, 7] 2 4"
-      (let [myArray [1, 2, 3, 4, 5, 6, 7]]
-        (should= [3 4 5] (file-io/partial-content myArray 2 5))))))
