c0317bcefbf1ad510895a4be0d879d4dd1e3d6b1
Tyler Decker <tyler.decker@gmail.com>
2016-08-10 18:19:59 -0500

    Removed dead code

--- spec/httpserver/file_io/file_input_spec.clj
+++ spec/httpserver/file_io/file_input_spec.clj
@@ -7,15 +7,4 @@
     (it "returns contents of testfile as byte-array"
         (should= (type (byte-array 3)) (type (file-input/get-file-contents-as-bytes
                               "./folder_for_io_testing"
-                              "/test.txt"))))
-   (it "returns a byte-array of length 34 when asked to open test.txt file"
-     (should= 34 (count (file-input/get-file-contents-as-bytes
-                            "./folder_for_io_testing"
-                            "/test.txt")))))
-
-  (describe "partial content"
-    (it "return a byte-array of the sting 'file' for range 8 12"
-      (should= "file" (apply str (map char (file-input/get-file-partial-content
-                                              "./folder_for_io_testing"
-                                              "/test.txt"
-                                              10 14)))))))
+                              "/test.txt"))))))
