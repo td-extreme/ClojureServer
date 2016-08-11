b70ac3ae69c98ec862a02f3c255a5b84326a5084
Tyler Decker <tyler.decker@gmail.com>
2016-08-11 09:35:10 -0500

    Added fallback if no header for Host: is provided

--- spec/httpserver/handlers/redirect_spec.clj
+++ spec/httpserver/handlers/redirect_spec.clj
@@ -8,7 +8,9 @@
                  :headers {"Host" "localhost:5000"}}]
     (it "returns a map with :code 302"
       (should= 302 (:code (redirect/call request))))
-    (it "returns a map with :headers {'Location' 'http://localhost:5000/'"
+    (it "returns a map with :headers {'Location' 'http://localhost:5000/' if request has header 'Host' 'localhost:5000'"
       (should= {"Location" "http://localhost:5000/"} (:headers (redirect/call request))))
+    (it "returns a mapt with :headers {'Location' '/' if no 'Host' header in request"
+      (should= {"Location" "/"} (:headers (redirect/call {:method "GET" :path "/redirect" :headers {}}))))
     (it "returns an emtpy body"
       (should= "" (:body (redirect/call request))))))
--- src/httpserver/handlers/redirect.clj
+++ src/httpserver/handlers/redirect.clj
@@ -1,4 +1,6 @@
 (ns httpserver.handlers.redirect)
 
 (defn call [request]
-  {:code 302 :headers {"Location" (str "http://" (get (:headers request) "Host") "/")} :body ""})
+  (cond
+    (= nil (get (:headers request) "Host")) {:code 302 :headers {"Location" "/" } :body ""}
+    :else   {:code 302 :headers {"Location" (str "http://" (get (:headers request) "Host") "/")} :body ""}))
