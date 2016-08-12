be2cb1dbcc0072ba2d147930722b05a81e91d223
Tyler Decker <tyler.decker@gmail.com>
2016-08-12 10:56:35 -0500

    Added delete_form handler

---
+++ src/httpserver/handlers/delete_form.clj
@@ -0,0 +1,10 @@
+(ns httpserver.handlers.delete-form
+  (:require [clojure.java.io :as io]))
+
+(defn delete [working-directory path]
+  (io/delete-file (str working-directory path)))
+
+(defn call [request working-directory]
+  (delete working-directory (:path request))
+  {:code 200 :headers {} :body ""})
+
