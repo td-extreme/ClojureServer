9feb4f5d187f0b025f260a6093ef884f28570423
Tyler Decker <tyler.decker@gmail.com>
2016-08-22 14:01:15 -0500

    update tests

--- spec/httpserver/handlers/get_form_spec.clj
+++ spec/httpserver/handlers/get_form_spec.clj
@@ -1,17 +1,18 @@
 (ns httpserver.get-form-spec
   (:require [speclj.core :refer :all]
-            [httpserver.handlers.get-form :as get-form]))
+            [httpserver.handlers.get-form :as get-form]
+            [httpserver.file-io.file-input :as file-input]
+            [httpserver.file-io.file-type-checker :as file-type-checker]
+))
 
 (import '(java.io File))
 
-(describe "No form has been posted"
-  (it "returns a map with :code 200"
-    (should= 200 (:code (get-form/call :dummy-request "."))))
+(describe "Getting a form"
+  (context "No form has been posted"
+     (it "returns a map with :code 200"
+        (should= 200 (:code (get-form/call :dummy-request "."))))
 
-  (it "returns a map with :body ''"
-    (should= "" (:body (get-form/call :dummy-request "."))))
-
-   (it "returns a map with :code 200"
-    (should= "this" (:body (get-form/call :dummy-request "."))))
+      (it "returns a map with :body ''"
+        (should= "" (:body (get-form/call :dummy-request "."))))))
 
 
